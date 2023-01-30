9767a4445cb459b0b23578de90d4c317c9d6f7be
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-01-30 22:57:39 +0300

    Configurations tamamlandı.

--- BP.Api/Program.cs
+++ BP.Api/Program.cs
@@ -11,6 +11,21 @@ namespace BP.Api
 {
     public class Program
     {
+        private static IConfiguration Configuration
+        {
+            get
+            {
+                String env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                return new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+            }
+        }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -20,6 +35,7 @@ namespace BP.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseConfiguration(Configuration);
                     webBuilder.UseStartup<Startup>();
                 });
     }
