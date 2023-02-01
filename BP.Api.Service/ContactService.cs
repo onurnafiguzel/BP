c0e51eb54fea621e449cf31dbda3234e34557aac
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-02-02 00:55:21 +0300

    Manuel AutoMapper

--- BP.Api.Service/ContactService.cs
+++ BP.Api.Service/ContactService.cs
@@ -1,4 +1,5 @@
-﻿using BP.Api.Data.Models;
+﻿using AutoMapper;
+using BP.Api.Data.Models;
 using BP.Api.Models;
 using System;
 using System.Collections.Generic;
@@ -8,10 +9,18 @@ namespace BP.Api.Service
 {
     public class ContactService : IContactService
     {
+        private readonly IMapper mapper;
+
+        public ContactService(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
         public ContactDVO GetContactById(int id)
         {
             // Veritabanından kayıdın getirilmesi
-            return null;
+            Contact dbContact = GetDummyContact();
+            ContactDVO resultContact = mapper.Map<ContactDVO>(dbContact);
+            return resultContact;
         }
 
         private Contact GetDummyContact()
