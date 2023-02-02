deda2fe3288c724e9b58855d50e0692941f3d5df
Onur <58780366+onurnafiguzel@users.noreply.github.com>
2023-02-02 10:42:12 +0300

    ContactValidator for fuentValidation

---
+++ BP.Api/Validations/ContactValidator.cs
@@ -0,0 +1,17 @@
+﻿using BP.Api.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BP.Api.Validations
+{
+    public class ContactValidator : AbstractValidator<ContactDVO>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id 0'dan büyük olmalıdır.");
+        }
+    }
+}
